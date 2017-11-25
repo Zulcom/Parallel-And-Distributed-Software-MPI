7daf4e87359adff02d2a38ca932e08a343e6127a
Zulcom <s01@ngs.ru>
2017-11-26 01:51:40 +0700

    SMuravev Lab6

--- SMuravev-Lab6-point-to-point/SMuravev-Lab6-point-to-point.cpp
+++ SMuravev-Lab6-point-to-point/SMuravev-Lab6-point-to-point.cpp
@@ -61,7 +61,7 @@ int main(int argc, char *argv[])
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	// Номер узла
 	MPI_Comm_rank(MPI_COMM_WORLD, &node_num);
-	if (node_num == 0) start_time = MPI_Wtime();
+	if (0 == node_num) start_time = MPI_Wtime();
 	ierr = MPI_File_open(MPI_COMM_WORLD, filename, MPI_MODE_RDONLY, MPI_INFO_NULL, &file);
 	// Ловим ошибку открытия файла
 	if (ierr) {
@@ -73,58 +73,56 @@ int main(int argc, char *argv[])
 
 	// Считаем длину текста и сколько текста должно быть выдано каждому узлу
 	MPI_File_get_size(file, &filesize);
-	blocksize = filesize / size;
+	blocksize = filesize / (size-1);
 
 	// Создаём указатели на начало и конец у каждого узла
-	start = node_num * blocksize;
-	end = start + blocksize - 1+ overlap;
-
-
 	/* Выделяем память для блока*/
-	block = new char[blocksize + 1];
+	block = new char[blocksize + 1 + overlap];
 
 	//Читаем данные в блок
-	MPI_File_read_at_all(file, start, block, blocksize, MPI_CHAR, MPI_STATUS_IGNORE);
-
-	block[blocksize] = '\0';
-
-	if (node_num == 0) {
+	if (0 == node_num) {
+		text = new char[filesize];
+		MPI_File_read(file,text,filesize,MPI_CHAR,MPI_STATUS_IGNORE);
 		cout << "String '" << str << "' is being searched in file '" << filename << "'" << endl;
-	}
-
-	cout << "Length of file " << filesize << " is divided into blocksize " << blocksize << " for node " << node_num << endl;
-
-	// Задаём указатель на начало текста
-	text = &block[0];
-	while (text_pos <= (blocksize - str_len))
-	{
-		// Выбираем символ с позиции равной длине искомой -1 для зануления
-		// нулевой элемент
-		occ_char = text[text_pos + str_len - 1];
-
-		// Если последний символ шаблона совпадает с текущим символом в тексте и
-		// если символы впереди текущего в тексте содержит искомой, у нас есть совпадение.
-		// memcmp сравнивает символы впереди текущего с искомым в блоках длины  str_len -1.
-
-		if (occ_char == str[str_len - 1] && (memcmp(str, text + text_pos, str_len - 1) == 0))
+		for (int i = 1; i < size; i++)
 		{
-			temp_count++;
+			MPI_Send(text+blocksize*(i-1),i == size-1 ? blocksize - 1: blocksize-1+overlap, MPI_CHAR, i, 0, MPI_COMM_WORLD);
 		}
-
-		// Смотрим на таблицу совпадений и решаем как увеличивать текстовый указатель
-		text_pos += occ1[occ_char];
-
+		
 	}
+	else
+	{
+		cout << "Length of file " << filesize << " is divided into blocksize " << blocksize << " for node " << node_num << endl;
+		MPI_Recv(block, node_num == size-1 ? blocksize - 1 : blocksize - 1 + overlap, MPI_CHAR, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		block[blocksize] = '\0';
+		// Задаём указатель на начало текста
+		text = &block[0];
+		while (text_pos <= (blocksize - str_len))
+		{
+			// Выбираем символ с позиции равной длине искомой -1 для зануления
+			// нулевой элемент
+			occ_char = text[text_pos + str_len - 1];
 
+			// Если последний символ шаблона совпадает с текущим символом в тексте и
+			// если символы впереди текущего в тексте содержит искомой, у нас есть совпадение.
+			// memcmp сравнивает символы впереди текущего с искомым в блоках длины  str_len -1.
+
+			if (occ_char == str[str_len - 1] && (memcmp(str, text + text_pos, str_len - 1) == 0))
+			{
+				temp_count++;
+			}
+
+			// Смотрим на таблицу совпадений и решаем как увеличивать текстовый указатель
+			text_pos += occ1[occ_char];
+		}			
+	}
 	// Добавляем  сумму каждого узла к общей сумме используя mpi_reduce
-
 	MPI_Reduce(&temp_count, &final_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-
 	if (node_num == 0) {
 		cout << "Total number of occurances of string in text = " << final_count << endl;
-		cout << "In " << MPI_Wtime() - start_time << " seconds" <<endl;
+		cout << "In " << (MPI_Wtime() - start_time) << " second" <<endl;
 	}
-
+	
 	MPI_File_close(&file);
 
 	MPI_Finalize();
