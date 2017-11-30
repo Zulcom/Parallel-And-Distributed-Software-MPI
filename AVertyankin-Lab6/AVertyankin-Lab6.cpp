2a4c5e354efb9cac67090131902ba8090835005e
Zulcom <s01@ngs.ru>
2017-12-01 04:19:33 +0700

    Lab6- w/o print

--- AVertyankin-Lab6/AVertyankin-Lab6.cpp
+++ AVertyankin-Lab6/AVertyankin-Lab6.cpp
@@ -24,38 +24,45 @@ int main(int argc, char** argv) {
 		fprintf(stderr, "World size must be greater than 3 for %s\n", argv[0]);
 		MPI_Abort(MPI_COMM_WORLD, 1);
 	}
+	int SIZE = 100000;
+	for (int i=100; i<1e7;i*=10)
+	{
+		SIZE = i;
+		char * msg = new char[SIZE + 1]; 
+		if (proc_num == 0) {
+			double start_time = MPI_Wtime();
+			char *table = "ZYXWVUTSRQPONMLKJIHGFEDCBA"; //27
+			for (int i = 0; i < SIZE; ++i)
+				msg[i] = 'A' + (rand() % 26);
+			msg[SIZE] = '\0';
+			//printf("Process 0 start program with \"%s\" \n", msg);
+			int blocksize = SIZE / (world_size - 1);
+			for (int i = 1; i < world_size; ++i) {
+				MPI_Send(&blocksize, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+				MPI_Send(msg + blocksize*(i - 1), blocksize, MPI_CHAR, i, 0, MPI_COMM_WORLD);
+				MPI_Send(table, 27, MPI_CHAR, i, 0, MPI_COMM_WORLD);
+			}
+			//printf("Result:");
+			for (int i = 1; i < world_size; ++i) {
+				char *rcvmsg = new char[blocksize];
+				MPI_Recv(rcvmsg, blocksize + 1, MPI_CHAR, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+				//printf("%s", rcvmsg);
+			}
+			printf("%d %f\n",i, MPI_Wtime() - start_time);
+		}
+		else {
 
-	char * msg = new char[world_size+1];
-	if (proc_num == 0) {
-		char *table =  "ZYXWVUTSRQPONMLKJIHGFEDCBA"; //27
-		for(int i=0;i<100;++i)
-			msg[i] = 'A' + (rand() % 26);
-		    msg[100] = '\0'; 
-		printf("Process 0 start program with \"%s\" \n", msg);
-		int blocksize = 100 / (world_size-1);
-		for (int i = 1; i < world_size; ++i) {
-			MPI_Send(&blocksize, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-			MPI_Send(msg + blocksize*(i-1), blocksize, MPI_CHAR, i, 0, MPI_COMM_WORLD);
-			MPI_Send(table, 27, MPI_CHAR, i, 0, MPI_COMM_WORLD);
+			int blocksize = 0;
+			char *table = new char[27];
+			MPI_Recv(&blocksize, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			char * msg = new char[blocksize + 1];
+			MPI_Recv(msg, blocksize, MPI_CHAR, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			msg[blocksize] = '\0';
+			MPI_Recv(table, 27, MPI_CHAR, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			//printf("Process %d received %s from proccess %d \n", proc_num, msg,status.MPI_SOURCE);
+			for (int i = 0; i < blocksize; ++i) msg[i] = table[msg[i] - 'A'];
+			MPI_Send(msg, blocksize + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
 		}
-		for (int i = 1; i < world_size; ++i) {
-			char *rcvmsg = new char[blocksize];
-			MPI_Recv(rcvmsg, blocksize+1, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-			printf("Process 0 received %s from process %d \n", rcvmsg, status.MPI_SOURCE);
-		}
-	}
-	else {
-		
-		int blocksize = 0;
-		char *table = new char[27];
-		MPI_Recv(&blocksize, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		char * msg = new char[blocksize+1];
-		MPI_Recv(msg, blocksize, MPI_CHAR, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-		msg[blocksize] = '\0';
-		MPI_Recv(table, 27, MPI_CHAR, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		printf("Process %d received %s from proccess %d \n", proc_num, msg,status.MPI_SOURCE);
-		for (int i = 0; i < blocksize; ++i) msg[i] = table[msg[i] - 'A'];
-		MPI_Send(msg, blocksize+1, MPI_CHAR,0, 0, MPI_COMM_WORLD);
 	}
 	MPI_Finalize();
 }
